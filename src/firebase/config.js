32480e45797954ea8dcd4390f58bbdc001f31d8b
maan <pranijman@gmail.com>
2021-12-27 18:14:11 +0545

    179

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBliEEPmbjOvqZPSqfq0GJpCVrrAlsTyvU",
@@ -15,5 +16,6 @@ initializeApp(firebaseConfig)
 
 // init services
 const db = getFirestore()
+const auth = getAuth()
 
-export { db }
+export { db, auth }
