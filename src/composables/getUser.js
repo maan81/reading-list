9fd43567b3e89e583e82f4d929fa8922dbf6e93e
maan <pranijman@gmail.com>
2021-12-29 16:10:23 +0545

    183 working

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -2,13 +2,13 @@ import { ref } from 'vue'
 
 // firebase imports
 import { auth } from '@/firebase/config'
-import { onAuthStageChanged } from 'firebase/auth'
+import { onAuthStateChanged } from 'firebase/auth'
 
 // refs
 const user = ref(auth.currentUser)
 
 // auth hanges
-onAuthStageChanged(auth, (_user) => {
+onAuthStateChanged(auth, (_user) => {
   console.log('User state changed. Current user is:', _user)
   user.value = _user
 })
